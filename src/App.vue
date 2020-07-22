1d6496cd4b5040cd34705bbc4133e74076c9304c
Evgeniy Lobanov <dirtha@yandex.ru>
2020-07-23 02:40:02 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="toolbar">
+      <p class="toolbar__title">Курс {{ current }} на сегодня</p>
+      <button type="button" class="toolbar__button toolbar__button_slide toolbar__button_slide_left">L</button>
+      <div class="toolbar__tabs">
+        <button
+          type="button"
+          class="toolbar__button toolbar__button_tab"
+          v-bind:class="{ 'toolbar__button_tab_active':(item === current) }"
+          v-for="item in currencies"
+          v-bind:key="item"
+          v-on:click="getRate(item)"
+        >{{ item }}</button>
+      </div>
+      <button type="button" class="toolbar__button toolbar__button_slide toolbar__button_slide_right">R</button>
+    </div>
+    <tab v-bind:currency="current" v-bind:rate="rate"></tab>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tab from "./components/Tab.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Tab
+  },
+  data() {
+    return {
+      currencies: [],
+      current: "",
+      rate: {},
+      numberOfTabs: 7,
+      serverError: ""
+    };
+  },
+  methods: {
+    getRate(currency) {
+      this.current = currency;
+      fetch(`https://api.openrates.io/latest?base=${currency}`)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`${res.status} ${res.statusText}`);
+        })
+        .then(data => {
+          this.rate = data.rates;
+        })
+        .catch(err => {
+          this.serverError = err;
+        });
+    }
+  },
+  created() {
+    fetch("https://api.openrates.io/latest")
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`${res.status} ${res.statusText}`);
+      })
+      .then(data => {
+        this.currencies = Object.keys(data.rates);
+        this.currencies.unshift(data.base);
+        this.current = data.base;
+        this.rate = data.rates;
+      })
+      .catch(err => {
+        this.serverError = err;
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 720px;
+  margin: auto;
 }
+
+.toolbar {
+  box-sizing: border-box;
+  height: 111px;
+  padding-top: 30px;
+  background-color: #ffe782;
+}
+
+.toolbar__title {
+  margin-left: 24px;
+  margin-top: 0;
+  margin-bottom: 8px;
+}
+
+.toolbar__tabs {
+  width: 630px;
+  height: 48px;
+  display: inline-block;
+  overflow: hidden;
+}
+
+.toolbar__button {
+  cursor: pointer;
+  color: black;
+  border: none;
+}
+
+.toolbar__button_tab {
+  width: 90px;
+  height: 48px;
+  background-color: #ffe782;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+
+.toolbar__button_tab_active {
+  background-color: white;
+}
+
+.toolbar__button_slide {
+  width: 45px;
+}
+
+.toolbar__button_slide_left {
+  background-image: url();
+}
+
+.toolbar__button_slide_right {
+  background-image: url();
+}
+
 </style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="results-form">
+    <input type="text" class="results-form__input" v-model="factor">
+    <p v-for="item in Object.entries(rate)" v-bind:key="item[0]">{{ item[0] }}:{{ item[1] * factor}}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["currency", "rate"],
+  data() {
+    return {
+      factor: 1
+    }
+  }
+};
+</script>
+
+<style>
+.results-form {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: 582px;
+}
+</style>
